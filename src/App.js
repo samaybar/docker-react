4ffd5e92c7f61a186cc22015f1969b71d1cc41dc
Sam Aybar <sam@polyscale.ai>
2024-03-08 16:21:56 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there react friends!
         </p>
         <a
           className="App-link"
